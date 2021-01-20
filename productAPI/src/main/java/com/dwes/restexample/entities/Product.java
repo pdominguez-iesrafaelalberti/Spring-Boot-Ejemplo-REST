2616ebd1075500322f040ed83e3d9cf6cb1e8a36
Juan Pablo Domínguez Mayo <72070792+pdominguez-iesrafaelalberti@users.noreply.github.com>
2021-01-20 19:29:10 +0100

    Update Product.java

--- productAPI/src/main/java/com/dwes/restexample/entities/Product.java
+++ productAPI/src/main/java/com/dwes/restexample/entities/Product.java
@@ -12,38 +12,30 @@ public class Product {
 	private int id;
 	private String name;
 	private String description;
-	private int price;
-
+	private Integer price;
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-	public int getPrice() {
+	public Integer getPrice() {
 		return price;
 	}
-
-	public void setPrice(int price) {
+	public void setPrice(Integer price) {
 		this.price = price;
 	}
-
 }
